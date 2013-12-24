9bb8e7a5144dcba481a138fe5827f6dc713dd61a
Jerry Jernej Kelih <jerry@jerrykelih.com>
2013-12-24 18:09:16 +0100

    new site

--- js/script.js
+++ js/script.js
@@ -1,39 +1,28 @@
-(function($) {
-    $(function() {
+$(document).ready(function() {
 
-        // PageTransitions
+    var iframe = $('#pPlayer')[0];
+    var player = $f(iframe);
+    player.addEvent('ready', function() {});
 
-        $(window).resize(function() {
-            var maxH = 0;
-            $('.tpl-10-section-1 .pt-page').css('height', 'auto').each(function() {
-                var h = $(this).outerHeight();
-                if (h > maxH)
-                    maxH = h;
-            }).css('height', maxH + 'px');
-            $('.tpl-10-section-1 .page-transitions').css('height', maxH + 'px');
+    function addEvent(element, eventName, callback) {
+        if (element.addEventListener) {
+            element.addEventListener(eventName, callback, false);
+        } else {
+            element.attachEvent(eventName, callback, false);
+        }
+    }
+
+
+    $('#play').click(function(evt) {
+        evt.preventDefault();
+        $('.mask').fadeIn('slow');
+        $('.popup-video').fadeIn('slow');
+        player.api('play')
+        $('.mask').click(function() {
+            player.api('pause');
+            $('.popup-video').fadeOut('slow');
+            $('.mask').fadeOut('slow');
+
         });
-
-        var pt2 = PageTransitions();
-        pt2.init('#pt-2');
-
-        $('#pt-2 .pt-control-prev').on('click', function() {
-            pt2.gotoPage(33, 'prev');
-            return false;
-        });
-
-        $('#pt-2 .pt-control-next').on('click', function() {
-            pt2.gotoPage(32, 'next');
-            return false;
-        });
-
-        $(window).resize().scroll();
-        // add class "loaded" here to force ani start
-        setTimeout(function() {
-            $('html').addClass('loaded');
-        }, 500);
     });
-
-    $(window).load(function() {
-        $(window).resize().scroll();
-    });
-})(jQuery);
+});
